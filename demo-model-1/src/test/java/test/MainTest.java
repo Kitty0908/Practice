a25aed789f29db841580c9c59c861823c407d867
kitty0908 <wangdeshan315@gmail.com>
2020-12-06 14:24:07 +0800

    demo-model-1 update

--- demo-model-1/src/test/java/test/MainTest.java
+++ demo-model-1/src/test/java/test/MainTest.java
@@ -1,9 +1,12 @@
 package test;
 
+import com.wds.dao.CityDao;
+
 public class MainTest {
 
     public static void main(String[] args){
-
+        CityDao dao = new CityDao();
+        System.out.println(dao.selectOne(1));
     }
     
 }
