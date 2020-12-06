62326a25b8ae7f0d7e5c818a0ce3ffa9e129ce7e
kitty0908 <wangdeshan315@gmail.com>
2020-12-06 14:18:55 +0800

    demo-model-1 update

--- demo-model-1/src/main/java/com/wds/dao/CityDao.java
+++ demo-model-1/src/main/java/com/wds/dao/CityDao.java
@@ -1,4 +1,20 @@
 package com.wds.dao;
 
+import com.wds.domain.City;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+
 public class CityDao {
+
+    public City selectOne(Integer cityid){
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory factory = builder.build(is);
+        SqlSession session = factory.openSession(true);
+
+        return session.selectOne("selectOne",cityid);
+    }
 }
