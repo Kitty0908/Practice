faef5361515421b1ae2f6a5c388868f7ca81faee
kitty0908 <wangdeshan315@gmail.com>
2021-03-06 18:03:29 +0800

    spring-boot-04-mybatis-plus create

---
+++ spring-boot-04-mybatis-plus/src/main/java/com/wds/mybatisplus/SpringBoot04MybatisPlusApplication.java
@@ -0,0 +1,13 @@
+package com.wds.mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot04MybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot04MybatisPlusApplication.class, args);
+    }
+
+}
